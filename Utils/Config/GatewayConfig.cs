2615e1e37e1624ab596bc9870326a45741343f2d
Linus Weigand <ge63nat@mytum.de>
2022-11-09 16:17:20 +0100

    init

---
+++ Utils/Config/GatewayConfig.cs
@@ -0,0 +1,13 @@
+public record GatewayConfig {
+    public string Url { get; set; } = "";
+    public int SessionTimeoutInMin { get; set; }
+    public string TokenExchangeStrategy { get; set; } = "";
+    public string Authority { get; set; } = "";
+    public string ClientId { get; set; } = "";
+    public string ClientSecret { get; set; } = "";
+    public string Scopes { get; set; } = "";
+    public string LogoutUrl { get; set; } = "";
+    public bool QueryUserInfoEndpoint { get; set; } = true;
+    // public string ApiScopes { get; set; } = "";
+    public ApiConfig[] ApiConfigs { get; set; } = {};
+}
